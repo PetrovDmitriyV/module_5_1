450e0e2f534bf9d173e876debaca6e5b4b404a3b
PetrovDmitriyV <phanl2@mail.ru>
2024-10-10 19:34:50 +0300

    Update module_5_1.py

--- module_5_1.py
+++ module_5_1.py
@@ -1,15 +1,21 @@
 class House:
     def __init__(self, name, number_of_floors):
         self.name = name
-        self.number_of_floors = int(number_of_floors)
+        self.number_of_floors = number_of_floors
+        if type(number_of_floors) != int:
+            print('Ошибка ввода данных')
+
 
     def go_to(self, new_floor):
-        self.new_floor = int(new_floor)
-        if new_floor <= self.number_of_floors and self.new_floor >= 1:
-            for i in range(1, self.new_floor + 1):
-                print(i)
+        self.new_floor = new_floor
+        if isinstance(new_floor, int):
+            if self.new_floor <= self.number_of_floors and self.new_floor >= 1:
+                for i in range(1, self.new_floor + 1):
+                    print(f'Лифт прибыл на {i} этаж')
+            else:
+                print('Такого этажа не существует')
         else:
-            print('Такого этажа не существует')
+            print('Ошибка ввода данных')
 
 
 h1 = House('ЖК Горский', 18)
